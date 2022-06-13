6dd9d20aef0c63ef9056b9af7f1d4b66b7177d76
Andrés notami <andresnotami@gmail.com>
2022-06-13 12:28:27 -0500

    🚧 Work in Progress:  Funciones

---
+++ src/12-functions.ts
@@ -0,0 +1,44 @@
+(() => {
+  type Sizes = 'S' | 'M' | 'L' | 'XL';
+
+  function createProductToJson(
+    title: string,
+    createAt: Date,
+    stock: number,
+    size: Sizes
+  ) {
+    return {
+      title,
+      createAt,
+      stock,
+      size
+    }
+  }
+
+  const product1 = createProductToJson('P1', new Date(), 5541, 'XL');
+  console.log(product1);
+  console.log(product1.title);
+  console.log(product1.stock);
+  console.log(product1.size);
+
+  const createProductToJsonV2 = (
+    title: string,
+    createAt: Date,
+    stock: number,
+    size?: Sizes
+  ) => {
+    return {
+      title,
+      createAt,
+      stock,
+      size
+    }
+  }
+
+  const product2 = createProductToJsonV2('P1', new Date(), 5541);
+  console.log(product2);
+  console.log(product2.title);
+  console.log(product2.stock);
+  console.log(product2.size);
+
+})();
